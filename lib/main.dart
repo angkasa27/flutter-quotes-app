c8efdffbf0c400031638e9aefa96bc6539387845
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2022-07-11 20:48:17 +0700

    Change to seprate class

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,37 @@ class QuotesList extends StatefulWidget {
 
 class _QuotesListState extends State<QuotesList> {
   List<Quote> quotes = [
-    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
-    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
-    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
+    Quote(
+        author: 'Oscar Wilde',
+        text: 'Be yourself; everyone else is already taken'),
+    Quote(
+        author: 'Oscar Wilde',
+        text: 'I have nothing to declare except my genius'),
+    Quote(
+        author: 'Oscar Wilde',
+        text: 'The truth is rarely pure and never simple')
   ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(quote.text,
+                style: TextStyle(fontSize: 18, color: Colors.grey[600])),
+            SizedBox(height: 6),
+            Text(
+              quote.author,
+              style: TextStyle(fontSize: 14, color: Colors.grey[800]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +56,9 @@ class _QuotesListState extends State<QuotesList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        children: quotes
+            .map((quote) => quoteTemplate(quote))
+            .toList(),
       ),
     );
   }
