6ad193e55cc87f38073b7be9e1ec3c3f14c43f5b
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2022-07-11 14:43:51 +0700

    Change to seprate class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
       home: QuotesList(),
@@ -12,10 +13,10 @@ class QuotesList extends StatefulWidget {
 }
 
 class _QuotesListState extends State<QuotesList> {
-  List<String> quotes = [
-    'Be yourself,; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The truth is rarely pure and never simple',
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
   ];
 
   @override
@@ -28,7 +29,7 @@ class _QuotesListState extends State<QuotesList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
       ),
     );
   }
---
+++ lib/quote.dart
@@ -0,0 +1,10 @@
+class Quote {
+
+  String text = '', author = '';
+
+  // Quote(String text, String author){
+  //   this.text = text;
+  //   this.author = author;
+  // }
+  Quote({required this.text, required this.author});
+}
