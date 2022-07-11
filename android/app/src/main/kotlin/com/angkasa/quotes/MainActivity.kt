698c513004ada446f960d65f6fb139f9b71fb642
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2022-07-11 14:31:47 +0700

    Create List Quotes app

---
+++ android/app/src/main/kotlin/com/angkasa/quotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.angkasa.quotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: QuotesList(),
+    ));
+
+class QuotesList extends StatefulWidget {
+  const QuotesList({Key? key}) : super(key: key);
+
+  @override
+  State<QuotesList> createState() => _QuotesListState();
+}
+
+class _QuotesListState extends State<QuotesList> {
+  List<String> quotes = [
+    'Be yourself,; everyone else is already taken',
+    'I have nothing to declare except my genius',
+    'The truth is rarely pure and never simple',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quote'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
+      ),
+    );
+  }
+}
